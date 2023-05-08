24bf31c5dd7d5a06e8db219efcae408726ecffb6
Rafael Soutto <rafaelsoutto@Rafaels-MacBook-Pro-2.local>
2023-05-08 12:03:00 -0300

    wip

--- zabbix/start_zabbix.sh
+++ zabbix/start_zabbix.sh
@@ -4,6 +4,18 @@ set -e
 
 source variables.sh
 
+export MYSQL_DATABASE=zabbix
+export MYSQL_USER=zabbix
+export MYSQL_PASSWORD=zabbix
+export MYSQL_ROOT_PASSWORD=root_pwd
+export MYSQL_PROXY_DATABASE=zabbix_proxy
+export ZBX_SERVER_HOST=zabbix-server
+export ZBX_SERVER_NAME=Composed installation
+export ZBX_ALLOWEDIP=zabbix-server
+export ZBX_JAVAGATEWAY_ENABLE=true
+export ZBX_STARTJAVAPOLLERS=5
+export ZBX_ENABLE_SNMP_TRAPS=true
+
 sudo docker-compose pull
 sudo docker-compose up -d
 
