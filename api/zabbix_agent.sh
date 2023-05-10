29882667e63f18ae65dcf1213a97503b3008c5d4
Rafael Soutto <rafaelsoutto@Rafaels-MacBook-Pro-2.local>
2023-05-10 17:11:28 -0300

    wip

--- api/zabbix_agent.sh
+++ api/zabbix_agent.sh
@@ -22,7 +22,7 @@ sudo apt-get remove zabbix-agent
 
 sudo apt update
 wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb
-sudo dpkg -i zabbix-release_5.0-1+focal_all.deb
+sudo dpkg-reconfigure -i zabbix-release_5.0-1+focal_all.deb
 sudo apt install -y zabbix-agent
 
 sudo apt update
