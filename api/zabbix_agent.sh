8e106f5601373f5b8e14dd2987ba8e4c530d2bbb
Rafael Soutto <rafaelsoutto@Rafaels-MacBook-Pro-2.local>
2023-05-10 17:00:30 -0300

    wip

--- api/zabbix_agent.sh
+++ api/zabbix_agent.sh
@@ -33,9 +33,45 @@ local_ip_address=$(echo "$ipconfig" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 echo $local_ip_address
 
 # Update zabbix_agentd.conf file
-sudo sed -i "s/^Server=.*$/Server=$ZABBIX_SERVER_IP/g" /etc/zabbix/zabbix_agentd.conf
-sudo sed -i "s/^ServerActive=.*$/ServerActive=$ZABBIX_SERVER_IP/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/^Server=.*$/Server=$ZABBIX_SERVER_IP,127.0.0.1/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/^ServerActive=.*$/ServerActive=$ZABBIX_SERVER_IP,127.0.0.1/g" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "s/^Hostname=.*$/Hostname=$local_ip_address/g" /etc/zabbix/zabbix_agentd.conf
 
 sudo systemctl restart zabbix-agent
 sudo systemctl status zabbix-agent
+
+
+curl -X POST -H 'Content-Type: application/json' -d '{
+    "jsonrpc": "2.0",
+    "method": "host.create",
+    "params": {
+        "host": "'"$local_ip_address"'",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "'"$local_ip_address"'",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 1,
+        "inventory": {
+            "location_lat": "56.95387",
+            "location_lon": "24.22067"
+        }
+    },
+    "auth": "2a937615efa5ca80e0e4bb5fed91391c8a1b0e99884a78ad0ee9ce400e7c6bec",
+    "id": "1"
+}' http://3.95.188.21:8080/api_jsonrpc.php
