6195ce17c694b7520d8c38837859599f62763910
Rafael Soutto <rafaelsoutto@Rafaels-MacBook-Pro-2.local>
2023-05-10 13:08:12 -0300

    wip

--- api/zabbix_agent.sh
+++ api/zabbix_agent.sh
@@ -14,6 +14,9 @@ done
 sudo apt update
 wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+focal_all.deb
 sudo dpkg -i zabbix-release_5.0-1+focal_all.deb
+sudo apt install zabbix-agent
+sudo systemctl status zabbix-agent
+
 sudo apt update
 
 sudo apt install -y net-tools
@@ -24,3 +27,5 @@ export local_ip_address=$(echo "$ipconfig" | grep -oP '(?<=inet\s)\d+(\.\d+){3}'
 sudo sed -i 's/^Server=.*/Server=<${ZABBIX_SERVER_IP}>/g' /etc/zabbix/zabbix_agentd.conf
 sudo sed -i 's/^ServerActive=.*/ServerActive=<${ZABBIX_SERVER_IP}>/g' /etc/zabbix/zabbix_agentd.conf
 sudo sed -i 's/^Hostname=.*/Hostname=<${local_ip_address}>/g' /etc/zabbix/zabbix_agentd.conf
+
+sudo systemctl restart zabbix-agent
