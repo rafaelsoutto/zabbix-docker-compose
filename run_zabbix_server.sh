c6c64666a20bc3bb329912d5504fa2e4dabc2ad5
Rafael Soutto <rafaelsoutto@Rafaels-MacBook-Pro-2.local>
2023-05-12 20:29:42 -0300

    wip

--- run_zabbix_server.sh
+++ run_zabbix_server.sh
@@ -4,13 +4,6 @@ set -e
 
 cd zabbix
 
-read -p "Do you want to skip Docker install? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Skipping Docker install."
-else
-  echo "Installing Docker..."
-  bash install.sh
-fi
+bash install.sh
 
 bash start_zabbix.sh
