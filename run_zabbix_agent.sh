649340e4140174a376fc0244d1b72919f933c69c
Rafael Soutto <rafaelsoutto@Rafaels-MacBook-Pro-2.local>
2023-05-12 20:31:12 -0300

    wip

--- run_zabbix_agent.sh
+++ run_zabbix_agent.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-read -p "Do you want to skip ZABBIX install? (y/n) " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Skipping ZABBIX install."
-else
-  echo "Installing ZABBIX..."
-  bash api/scripts/dependencies_install.sh
-fi
+bash api/scripts/dependencies_install.sh
 
 source api/scripts/get_network_params.sh
